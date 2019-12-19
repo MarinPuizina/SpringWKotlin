72cb0b01235a50401b4b9dbd78df9d5c90516edb
MarinPuizina <marinpuizina@hotmail.com>
2019-12-19 02:13:49 +0100

    Add creating user in db and getting all the users from db

--- src/main/kotlin/com/marin/swk/UserController.kt
+++ src/main/kotlin/com/marin/swk/UserController.kt
@@ -2,23 +2,32 @@ package com.marin.swk
 
 import lombok.AllArgsConstructor
 import org.springframework.core.env.Environment
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/user")
-class UserController(val environment: Environment) {
+@RequestMapping("/users")
+class UserController(val environment: Environment, val userRepository: UserRepository) {
 
     @GetMapping("/status")
     fun getStatus(): String{
         return "Service is working on a port ${environment.getProperty("local.server.port")}"
     }
 
+    /*
     @GetMapping
     fun getUser(): User {
         return User("Marin", 12312312, "bla@bla.com")
+    }*/
+
+    @GetMapping
+    fun getUsers(): List<UserEntity> {
+        return userRepository.findAll()
+    }
+
+    @PostMapping
+    fun createUser(@RequestBody user: UserEntity): UserEntity {
+        return userRepository.save(UserEntity(user.id, user.name, user.email))
     }
 
 }
